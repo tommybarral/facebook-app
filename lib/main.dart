24398dbd1e74f1cabb4ce9547092cd4f1810076c
Tommybarral <isamutom27@hotmail.com>
2022-02-28 14:36:25 +0100

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Facebook app'),
+        centerTitle: true
+      ),
+      body: Center(child: Text('MainPage'),),
+    );
+  }
+}
